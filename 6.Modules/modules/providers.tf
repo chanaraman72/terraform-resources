dded70a92900bfaa2b1f1f8b694010ac55bc4ec1
chanaraman97 <66430954+chanaraman97@users.noreply.github.com>
2021-06-24 21:51:08 +0100

    Update providers.tf

--- 6.Modules/modules/providers.tf
+++ 6.Modules/modules/providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAXBJIVQHP56JWYQED"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
